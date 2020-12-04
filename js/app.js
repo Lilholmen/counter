f737e51e0db2e31dbfca4750c0acf9ffec281042
lilholmen <lilholmen@yandex.ru>
2020-12-05 00:15:56 +0300

    script section is done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let count = 0;
-const value = document.querySelector('.container___count-value');
+const value = document.querySelector('.container__count-value');
 const btns = document.querySelectorAll('.container__btn');
 
 btns.forEach((item) => {
@@ -9,6 +9,14 @@ btns.forEach((item) => {
     const styles = event.currentTarget.classList;
 
     if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
     }
+
+    value.style.color = count > 0 ? 'green' : count < 0 ? 'red' : '#102a42';
+    value.textContent = count;
   });
 });
